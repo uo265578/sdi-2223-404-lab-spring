c35b19bfea72b9468362630ff790fec596d4e82b
uo265578 <uo265578@uniovi.es>
2023-02-13 16:33:22 +0100

    SDI-2223-404-2.1-Create Spring project.

---
+++ src/test/java/com/uniovi/notaneitor/Sdi2223404SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaneitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223404SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
