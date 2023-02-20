1c2db36b5994abca8a8978bb280fed775c9a21c4
uo265578 <uo265578@uniovi.es>
2023-02-20 13:02:20 +0100

    SDI-2021-404-3.5-Validación de datos en el servidor.

--- src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
@@ -1,8 +1,11 @@
 package com.uniovi.notaneitor.controllers;
 import com.uniovi.notaneitor.services.SecurityService;
+import com.uniovi.notaneitor.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.notaneitor.entities.*;
 import com.uniovi.notaneitor.services.UsersService;
@@ -10,6 +13,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 @Controller
 public class UsersController {
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
     @Autowired
     private UsersService usersService;
     @Autowired
@@ -52,11 +57,23 @@ public class UsersController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+
+       if(result.hasErrors()) {
+           return "signup";
+       }
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
     }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
         return "login";
